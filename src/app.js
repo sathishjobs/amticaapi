27e7563125d68d9c99f138d020972d22b45374f1
DESKTOP-02EGQ03\MItosis <saranya.s@mitosistech.com>
2018-11-30 12:05:36 +0530

    style serving issue fixed;

--- src/app.js
+++ src/app.js
@@ -14,11 +14,8 @@ const app = express();
 //link middleware to app
 middleWareConfig(app);
 
-app.use(express.static('public'))
-
-app.get('/',(req,res)=>{
-  res.sendFile(path.join(__dirname + '/public/index.html'));
-})
+app.use(express.static(path.join(__dirname, '/public/static')));
+app.use("/",express.static(path.join(__dirname, 'public')));
 
 app.use("/user/",userRoutes.routes);
 
